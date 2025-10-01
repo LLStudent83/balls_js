a7237984cc210ba39db6acad9e28fc3de275f544
Prigulnov Dmitry <prigulnov@motionlogic.ru>
2025-10-01 16:27:42 +0500

    fix: обработана ошибка когда элемент с id root не найден

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import App from "app/App";
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+	const root = createRoot(rootElement);
+	root.render(<App />);
+} else {
+	throw new Error('Элемент с id "root" не найден');
+}
 
 if (module.hot) {
-  module.hot.accept();
+	module.hot.accept();
 }
