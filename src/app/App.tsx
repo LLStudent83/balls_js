8cc9cc4a4cb5cb4e7b14ef07f4904dbb54586711
Prigulnov Dmitry <prigulnov@motionlogic.ru>
2025-10-25 18:36:58 +0500

    config: установлен tailewindcss

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,16 @@
 import { GamePage } from "pages/gamePage";
 import { StrictMode } from "react";
-import "./styles/rootStyles.scss";
 import ApiProvider from "shared/api";
+import "./styles/rootStyles.css";
 
 function App() {
 	return (
 		<StrictMode>
 			<ApiProvider>
-				<GamePage />
+				{/* <GamePage /> */}
+				<h1 className="text-3xl font-bold underline text-black">
+					Hello world!
+				</h1>
 			</ApiProvider>
 		</StrictMode>
 	);
