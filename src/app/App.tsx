3a219b5477453ee4dabd9523431bf36d27d40acf
Prigulnov Dmitry <prigulnov@motionlogic.ru>
2025-10-27 11:00:23 +0500

    refactor: вернул компонент GabePage на место

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,12 @@ import { GamePage } from "pages/gamePage";
 import { StrictMode } from "react";
 import ApiProvider from "shared/api";
 import "./styles/rootStyles.css";
-import {Button} from "@shadcn/components/ui/button"
 
 function App() {
 	return (
 		<StrictMode>
 			<ApiProvider>
-				{/* <GamePage /> */}
-				<h1 className="text-3xl font-bold underline text-black">
-					Hello world!
-				</h1>
-<Button>Click me</Button>
+				<GamePage />
 			</ApiProvider>
 		</StrictMode>
 	);
