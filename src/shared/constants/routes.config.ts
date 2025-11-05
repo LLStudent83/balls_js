e5ebc456eb1e51623805b02bf640dae772da1b4d
Prigulnov Dmitry <prigulnov@motionlogic.ru>
2025-11-05 21:11:11 +0500

    feat: добавлены объекты для конфигурации маршрутов

---
+++ src/shared/constants/routes.config.ts
@@ -0,0 +1,27 @@
+type RoutesConfig<TRoutes extends Record<string, string>> = TRoutes & {
+	get withSlash(): Record<keyof TRoutes, string>;
+};
+
+function createRoutes<const TRoutes extends Record<string, string>>(
+	routes: TRoutes,
+): RoutesConfig<TRoutes> {
+	const routesStorage = routes;
+
+	return {
+		...routes,
+		get withSlash() {
+			return Object.fromEntries(
+				Object.entries(routesStorage).map(([key, value]) => [key, `/${value}`]),
+			) as Record<keyof TRoutes, string>;
+		},
+	};
+}
+
+export const authRoutes = createRoutes({
+	register: "register",
+	login: "login",
+});
+
+export const gameRoutes = createRoutes({
+	game: "game",
+});
