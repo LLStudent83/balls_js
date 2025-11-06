fb792094f853f4c9509e789f27d241ee3bcd8b71
Prigulnov Dmitry <prigulnov@motionlogic.ru>
2025-11-06 18:09:15 +0500

    refactor: добавлена обработка успешного входа и ошибок в компоненте LoginPage

--- src/pages/loginPage/ui/LoginPage.tsx
+++ src/pages/loginPage/ui/LoginPage.tsx
@@ -1,11 +1,33 @@
+import { useUserStore } from "entities/User";
+import { useNavigate } from "react-router";
 import { type LoginDto, useAuthControllerLogin } from "shared/api";
+import { gameRoutes } from "shared/constants/routes.config";
 import { LoginForm } from "./LoginForm";
 
 export function LoginPage() {
 	const register = useAuthControllerLogin();
+	const setUser = useUserStore((state) => state.setUser);
+	const navigate = useNavigate();
+
+	const authHandler = (userData) => {
+		const { id, nickName, email } = userData;
+		setUser({ userId: id, nickName, email });
+
+		navigate(gameRoutes.withSlash.game, { viewTransition: true });
+	};
+
+	const onLoginError = (error: any) => {
+		console.error("Ошибка входа:", error);
+	};
 
 	const loginHandler = (userData: LoginDto) => {
-		register.mutate({ data: userData });
+		register.mutate(
+			{ data: userData },
+			{
+				onSuccess: authHandler,
+				onError: onLoginError,
+			},
+		);
 	};
 
 	return (
