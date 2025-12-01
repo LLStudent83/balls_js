c4f87ffb87ad566345a835525b029d385a7771a7
Prigulnov Dmitry <prigulnov@motionlogic.ru>
2025-12-01 16:31:16 +0500

    feat: добавлена стилизация активных ссылок и подчеркивание по hover

--- src/shared/ui/layout/Layout.tsx
+++ src/shared/ui/layout/Layout.tsx
@@ -1,16 +1,27 @@
-import { Link, Outlet } from 'react-router';
+import { NavLink, Outlet } from 'react-router';
 import { authRoutes, gameRoutes, rulesRoutes } from 'shared/config/routes/routes.config';
+import clsx from 'clsx';
 
 export function Layout() {
+  const getClassesForLink = ({ isActive }: { isActive: boolean }) => {
+    return clsx({ 'font-bold': isActive }, 'hover:underline');
+  };
+
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60 px-6 flex justify-between items-center">
         <div className="flex h-14 items-center gap-3">
-          <Link to={rulesRoutes.withSlash.rules}>Правила игры</Link>
-          <Link to={gameRoutes.withSlash.game}>Играть</Link>
+          <NavLink className={getClassesForLink} to={rulesRoutes.withSlash.rules}>
+            Правила игры
+          </NavLink>
+          <NavLink className={getClassesForLink} to={gameRoutes.withSlash.game}>
+            Играть
+          </NavLink>
         </div>
         <div>
-          <Link to={authRoutes.withSlash.register}>Вход/Регистрация</Link>
+          <NavLink className={getClassesForLink} to={authRoutes.withSlash.register}>
+            Вход/Регистрация
+          </NavLink>
         </div>
       </header>
 
