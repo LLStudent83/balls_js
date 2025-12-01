47088118408ddad7284b46a8c016ee959979e65c
Prigulnov Dmitry <prigulnov@motionlogic.ru>
2025-12-01 16:01:23 +0500

    feat: в header добавлена кнопка перехода к регистрации

--- src/shared/ui/layout/Layout.tsx
+++ src/shared/ui/layout/Layout.tsx
@@ -1,12 +1,15 @@
 import { Link, Outlet } from 'react-router';
+import { authRoutes, rulesRoutes } from 'shared/config/routes/routes.config';
 
 export function Layout() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
-      {/* Header */}
-      <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60 px-5">
-        <div className="container flex h-14 items-center">
-          <Link to="/rules">Правила игры</Link>
+      <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60 px-6 flex justify-between items-center">
+        <div className="flex h-14 items-center">
+          <Link to={rulesRoutes.withSlash.rules}>Правила игры</Link>
+        </div>
+        <div>
+          <Link to={authRoutes.withSlash.register}>Вход/Регистрация</Link>
         </div>
       </header>
 
